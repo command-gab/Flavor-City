c7f1f6a6e6873299ee20c64b1eeeb5501ac9fe74
roxanalemus <roxanalemusdev@gmail.com>
2021-11-20 00:10:11 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://commandgab:Testing4@cluster0.cev5m.mongodb.net/newGram?retryWrites=true&w=majority', 
+    'dbName': 'newGram'
+};
